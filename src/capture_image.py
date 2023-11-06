6852d004ee37f1cfd325132361ebe437f99321eb
AlexEspinosa98 <espinosaalexander09@gmail.com>
2023-11-06 11:44:07 -0500

    feat: add all flow to detetc morphology

--- src/capture_image.py
+++ src/capture_image.py
@@ -2,7 +2,8 @@
 This module is used to capture an image from the camera and save it in the specified route.
 """
 
-from camera_controller import CameraController
+# from camera_controller import CameraController
+from processing_image import Morfologydetection
 
 
 def capture() -> str:
@@ -13,15 +14,18 @@ def capture() -> str:
     Returns:
         str: The route (file path) of the saved image.
     """
-    # Create a CameraController object
-    camera_controller = CameraController()
+    # # Create a CameraController object
+    # camera_controller = CameraController()
 
-    # Change the route and raspberry_id fields
-    camera_controller.raspberry_id = "rpi_1"
+    # # Change the route and raspberry_id fields
+    # camera_controller.raspberry_id = "rpi_1"
 
-    # Capture and save an image
-    route = camera_controller.capture_and_save_image()
-
+    # # Capture and save an image
+    # route = camera_controller.capture_and_save_image()
+    # Path Alex relative
+    route = '../images/DSC_0088_JPG_jpg.rf.83725eb52fa35e5cc76786636486974f.jpg'
+    detection = Morfologydetection(route=route)
+    detection.predict_image()
     print("Image saved in: " + route)
 
     return route
--- src/processing_image.py
+++ src/processing_image.py
@@ -2,8 +2,66 @@
 This file is in charge of processing the image and returning a data structure with the information of the image
 """
 
-from capture_image import capture
+import numpy as np
+from ultralytics import YOLO
 
-# TODO: Create function to process the image and return a data structure with the information of the image
-route_img = capture()
-# route_img = "path/to/image.jpg"
+
+class Morfologydetection:
+    """
+    A class for performing morphology detection.
+
+    Args:
+        route (str): The image route.
+        raspberry_id (str): The Raspberry Pi ID.
+
+    Attributes:
+        router_image (str): The image route provided during initialization.
+        model (str): The relative path to the model file used for morphology detection.
+    """
+
+    def __init__(self, **args):
+        """
+        Initializes the Morfologydetection class.
+
+        Args:
+            route (str): The image route.
+            raspberry_id (str): The Raspberry Pi ID.
+        """
+        self.router_image = args['route']
+        # relative path for the model(change for rspberry)
+        self.model = YOLO('../model/best.pt')
+        self.calibrationx = 1
+        self.calibrationy = 2
+
+    def predict_image(self):
+        """
+        Predicts the image.
+
+        Returns:
+            dict: The dictionary with the image information.
+        """
+        results = self.model(self.router_image, imgsz=1024)
+
+        for result in results:
+            print(result.masks)
+            if result.masks:
+                response = self.extrac_morphology(result.masks)
+        return response
+
+    def extrac_morphology(self, maks):
+        response = {}
+        for id, recorrido in enumerate(maks.xy):
+            arr = np.array(recorrido)
+            max_column_1 = np.max(arr[:, 0])
+            max_column_2 = np.max(arr[:, 1])
+            min_column_1 = np.min(arr[:, 0])
+            min_column_2 = np.min(arr[:, 1])
+            print(max_column_1, max_column_2, min_column_1, min_column_2)
+            dif_y = max_column_1 - min_column_1
+            dif_x = max_column_2 - min_column_2
+            alto = dif_y * self.calibrationy
+            ancho = dif_x * self.calibrationx
+            print(alto, ancho)
+            response[f"data_{id}"] = {'alto': alto, 'ancho': ancho}
+
+        return response
